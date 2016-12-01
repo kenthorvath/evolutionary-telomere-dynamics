0e78b7ae8ff764fe4efdfe73b240864c378249dc
Kent Horvath <kenthorvath@gmail.com>
2016-12-01 00:43:15 -0800

    Specify bucket in job generator

--- src/main/scala/JobGenerator.scala
+++ src/main/scala/JobGenerator.scala
@@ -35,7 +35,7 @@ object JobGenerator {
       val counter: Int = Counter.next
       println(s"#!/bin/sh")
       println(s"scala -cp ../target/scala-2.11/pac_effect_2.11-1.0.jar Simulator $modelParameters ../sbatch-output/model-${counter}.csv &&")
-      println(s"aws s3 cp ../sbatch-output/model-${counter}.csv")
+      println(s"aws s3 cp ../sbatch-output/model-${counter}.csv s3://telomere-dynamics/modern-humans-cancer-scaling/")
       println(s"rm ../sbatch-output/model-${counter}.csv")
     }
 
