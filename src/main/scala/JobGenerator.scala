bfc0f477e4be0f9295523eacdcaeb9f556149a59
Kent Horvath <kenthorvath@gmail.com>
2017-01-16 11:16:13 -0800

    Add bucket name specificity to JobGenerator

--- src/main/scala/JobGenerator.scala
+++ src/main/scala/JobGenerator.scala
@@ -5,8 +5,8 @@
 object JobGenerator {
   def main(args: Array[String]): Unit = {
 
-    if (args.length < 2) {
-      throw new Exception("usage: <runLength> <trialCount>")
+    if (args.length < 3) {
+      throw new Exception("usage: <runLength> <trialCount> <bucket-name>")
     }
     else {
       ()
@@ -34,7 +34,7 @@ object JobGenerator {
       val counter: Int = Counter.next
       println(s"#!/bin/sh")
       println(s"scala -cp ../target/scala-2.11/pac-effect-assembly-1.0.jar Simulator $modelParameters ../sbatch-output/model-${counter}.csv &&")
-      println(s"aws s3 cp ../sbatch-output/model-${counter}.csv s3://telomere-dynamics/modern-humans-cancer-scaling/")
+      println(s"aws s3 cp ../sbatch-output/model-${counter}.csv s3://telomere-dynamics/${args(2)}")
       println(s"rm -f ../sbatch-output/model-${counter}.csv")
     }
 
